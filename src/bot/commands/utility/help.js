2d77be468866eb662c17ce28750f11a8d3b59aaf
Vecto <157636760+VectoDE@users.noreply.github.com>
2024-07-31 23:45:08 +0200

    Update help.js

--- src/bot/commands/utility/help.js
+++ src/bot/commands/utility/help.js
@@ -1,59 +1,21 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  data: {
-    name: 'help',
-    description: 'Listet alle verfügbaren Befehle nach Kategorien auf.',
-    category: 'Utility',
-  },
-  async execute(interaction) {
-    const { commands } = interaction.client;
-    const categories = new Set();
+    data: {
+        name: 'help',
+        description: 'Listet alle verfügbaren Befehle auf.',
+        category: 'Info',
+    },
+    async execute(interaction) {
+        const commands = interaction.client.commands.map(cmd => `\`${cmd.data.name}\`: ${cmd.data.description}`).join('\n');
 
-    console.log('Verfügbare Befehle:', [...commands.keys()]);
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('Befehle')
+            .setDescription('Hier sind die verfügbaren Befehle:')
+            .addField('Kommandos', commands)
+            .setTimestamp();
 
-    if (commands.size === 0) {
-      console.error('Keine Befehle gefunden. Überprüfe den Command Handler.');
-      await interaction.reply({ content: 'Keine Befehle gefunden. Überprüfe den Command Handler.', ephemeral: true });
-      return;
-    }
-
-    // Kategorien herausfinden
-    commands.forEach((command) => {
-      if (command.data && command.data.name) {
-        const category = command.data.category || 'Miscellaneous';
-        categories.add(category);
-      }
-    });
-
-    // Erstelle das Embed
-    const embed = new EmbedBuilder()
-      .setColor('#0099ff')
-      .setTitle('Verfügbare Befehle')
-      .setDescription('Hier sind die verfügbaren Befehle nach Kategorien:');
-
-    // Befehle nach Kategorien sortieren
-    let hasCommands = false; // Überprüft, ob Befehle hinzugefügt wurden
-
-    for (const category of categories) {
-      let commandsList = '';
-      commands.forEach((command) => {
-        if (command.data && command.data.category === category) {
-          commandsList += `\`${command.data.name}\`: ${command.data.description}\n`;
-          hasCommands = true;
-        }
-      });
-
-      if (commandsList) {
-        embed.addFields({ name: category, value: commandsList });
-      }
-    }
-
-    // Falls keine Befehle angezeigt werden, gebe eine entsprechende Nachricht aus
-    if (!hasCommands) {
-      embed.setDescription('Keine Befehle verfügbar.');
-    }
-
-    await interaction.reply({ embeds: [embed], ephemeral: true });
-  }
+        await interaction.reply({ embeds: [embed] });
+    },
 };
