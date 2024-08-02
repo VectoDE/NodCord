73b857c1d28c099fcf8151fd1d4d1371fb771bca
Vecto <157636760+VectoDE@users.noreply.github.com>
2024-08-03 01:25:32 +0200

    Update paymentModel.js

---
+++ src/models/paymentModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const paymentModel = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User', // Verweist auf das User-Modell, falls vorhanden
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    method: {
+        type: String,
+        required: true,
+        enum: ['credit_card', 'apple_pay', 'google_pay', 'amazon_pay', 'stripe', 'paypal', 'bank_transfer'],
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending',
+    },
+    transactionId: {
+        type: String,
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Payment', paymentModel);
