3a7b3be2cd77772355893da7aedf11885aaf0487
Vecto <system.bot.discord.utility@gmail.com>
2024-08-07 11:52:06 +0200

    0.2.2
    - Fix no model

---
+++ src/models/apiKeyModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ApiKeySchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  key: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('ApiKey', ApiKeySchema);
---
+++ src/models/developerProgramModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const DeveloperProgramSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  }
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('DeveloperProgram', DeveloperProgramSchema);
