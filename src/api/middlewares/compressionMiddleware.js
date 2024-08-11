27556815aa46f87cbd7d713ab8ef38f288e60998
Vecto <system.bot.discord.utility@gmail.com>
2024-08-11 20:00:25 +0200

    0.2.5

--- src/api/middlewares/compressionMiddleware.js
+++ src/api/middlewares/compressionMiddleware.js
@@ -2,14 +2,16 @@ const compression = require('compression');
 const logger = require('../services/loggerService');
 
 const compressionMiddleware = (req, res, next) => {
+  const compressionOptions = {
+    level: 6, // 0 (keine Komprimierung) bis 9 (maximale Komprimierung)
+    threshold: 1000, // > 1000 Bytes
+  };
+
   logger.info(
     `Compression Middleware: Processing request ${req.method} ${req.url} with compression level ${compressionOptions.level} and threshold ${compressionOptions.threshold} bytes`
   );
 
-  compression({
-    level: 6, // 0 (keine Komprimierung) bis 9 (maximale Komprimierung)
-    threshold: 1000, // > 1000 Bytes
-  })(req, res, (err) => {
+  compression(compressionOptions)(req, res, (err) => {
     if (err) {
       logger.error('Compression Middleware error:', err);
       return res.status(500).json({ message: 'Compression error' });
