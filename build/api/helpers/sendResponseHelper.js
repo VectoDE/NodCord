fdffd235df6a9205524a11249563d65d57508809
Vecto <157636760+VectoDE@users.noreply.github.com>
2024-09-20 17:25:57 +0200

    Delete build/api directory

--- build/api/helpers/sendResponseHelper.js
+++
@@ -1,2 +0,0 @@
-var t=(s,e,o,n)=>{if(!s||!e)throw new Error("Request or Response object is undefined");s.accepts("html")?e.redirect(o):e.json(n)};module.exports=t;
-//# sourceMappingURL=sendResponseHelper.js.map
--- build/api/services/appleService.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=appleService.js.map
