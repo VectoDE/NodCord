2be049165b3464241b605fd0b8662c43f96561bd
VectoDE <system.bot.discord.utility@gmail.com>
2025-10-23 00:24:24 +0200

    chore: add enterprise runtime configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,116 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
     node: true,
+    es2023: true,
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'script',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+  plugins: ['import', 'promise', 'security', 'sonarjs', 'prettier'],
   extends: [
     'eslint:recommended',
-    'plugin:node/recommended',
+    'plugin:import/recommended',
+    'plugin:promise/recommended',
+    'plugin:security/recommended-legacy',
+    'plugin:sonarjs/recommended',
     'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
   rules: {
     'prettier/prettier': 'error',
-    'no-console': 'warn',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'prefer-const': ['error', { destructuring: 'all' }],
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always'],
+    'promise/catch-or-return': ['error', { allowFinally: true }],
+    'security/detect-object-injection': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            prefer: 'type-imports',
+            fixStyle: 'inline-type-imports',
+          },
+        ],
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: {
+              arguments: false,
+              attributes: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['**/*.test.{ts,tsx,js}', '**/*.spec.{ts,tsx,js}'],
+      env: { jest: true },
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      rules: {
+        'sonarjs/no-duplicate-string': 'off',
+      },
+    },
+    {
+      files: ['**/*.d.ts'],
+      rules: {
+        'import/no-duplicates': 'off',
+      },
+    },
+  ],
 };
